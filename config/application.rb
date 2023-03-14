a7338806b66a556979a51c4969d9c8dbc1a5763d
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2023-03-14 14:15:41 +0100

    config: set the default locale to French
    And clue a French locale with it.

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module BetaRailsTemplate
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.i18n.default_locale = :fr
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
