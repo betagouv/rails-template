11d9a5793641baa0e530ebe3bf34988489b2c7d3
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2023-07-20 15:47:48 +0200

    rspec: allow filtering examples

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -53,8 +53,8 @@ RSpec.configure do |config|
   #   # is tagged with `:focus`, all examples get run. RSpec also provides
   #   # aliases for `it`, `describe`, and `context` that include `:focus`
   #   # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
-  #   config.filter_run_when_matching :focus
-  #
+  config.filter_run_when_matching :focus
+
   #   # Allows RSpec to persist some state between runs in order to support
   #   # the `--only-failures` and `--next-failure` CLI options. We recommend
   #   # you configure your source control system to ignore this file.
