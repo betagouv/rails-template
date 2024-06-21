241aba1833549b21f75ec30b83c3e487173d8adc
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2024-06-21 11:19:23 +0200

    docker: don't include mailcatcher
    never actually used it

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,10 +48,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # send emails to MailCatcher
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: "mailcatcher", port: 1025 }
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
